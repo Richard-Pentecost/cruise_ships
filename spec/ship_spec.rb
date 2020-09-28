d75341bf70ff3819de2febf98b41c39f067054b9
Richard Pentecost <richard.pentecost@madetech.com>
2020-09-28 08:03:30 +0100

    refactor ship tests with a before(:each) hook

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,70 +3,52 @@ require_relative '../port'
 require_relative '../itinerary'
 
 describe Ship do
+  before(:each) do
+    @dover = Port.new('Dover')
+    @calais = Port.new('Calais')
+    itinerary = Itinerary.new([@dover, @calais])
+    @ship = described_class.new(itinerary)
+  end
+
   context 'ship' do
     it 'initiates a ship with a starting port' do
-      dover = Port.new('Dover')
-      calais = Port.new('Calais')
-      itinerary = Itinerary.new([dover, calais])
-      ship = described_class.new(itinerary)
-
-      expect(ship.current_port).to be(dover)
+      expect(@ship.current_port).to be(@dover)
     end
 
     it 'gets added to a port on instantiation' do
-      dover = Port.new('Dover')
-      calais = Port.new('Calais')
-      itinerary = Itinerary.new([dover, calais])
-      ship = described_class.new(itinerary)
-      
-      expect(dover.ships).to include(ship)
+      expect(@dover.ships).to include(@ship)
     end
 
     it 'initiates a ship with a previous port of nil' do
-      dover = Port.new('Dover')
-      calais = Port.new('Calais')
-      itinerary = Itinerary.new([dover, calais])
-      ship = described_class.new(itinerary)
-
-      expect(ship.previous_port).to eq(nil)
+      expect(@ship.previous_port).to eq(nil)
     end
 
     it 'can set sail' do
-      dover = Port.new('Dover')
-      calais = Port.new('Calais')
-      itinerary = Itinerary.new([dover, calais])
-      ship = described_class.new(itinerary)
+      @ship.set_sail
 
-      ship.set_sail
-
-      expect(ship.previous_port).to be(dover)
-      expect(ship.current_port).to eq('')
-      expect(dover.ships).to_not include(ship)
+      expect(@ship.previous_port).to be(@dover)
+      expect(@ship.current_port).to eq('')
+      expect(@dover.ships).to_not include(@ship)
     end
 
     it 'can dock at a different port' do
-      dover = Port.new('Dover')
-      calais = Port.new('Calais')
-      itinerary = Itinerary.new([dover, calais])
-      ship = described_class.new(itinerary)
+      @ship.set_sail
+      @ship.dock
 
-      ship.set_sail
-      ship.dock
-
-      expect(ship.current_port).to be(calais)
-      expect(calais.ships).to include(ship)
+      expect(@ship.current_port).to be(@calais)
+      expect(@calais.ships).to include(@ship)
     end
 
     it "can't sail further than it's last port in the itinerary" do
-      dover = Port.new('Dover')
-      calais = Port.new('Calais')
-      itinerary = Itinerary.new([dover, calais])
-      ship = described_class.new(itinerary)
+      # dover = Port.new('Dover')
+      # calais = Port.new('Calais')
+      # itinerary = Itinerary.new([dover, calais])
+      # ship = described_class.new(itinerary)
 
-      ship.set_sail
-      ship.dock
+      @ship.set_sail
+      @ship.dock
 
-      expect{ ship.set_sail }.to raise_error(Exception)
+      expect{ @ship.set_sail }.to raise_error(Exception)
     end
 
 
