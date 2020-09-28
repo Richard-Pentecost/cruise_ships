9ffda9a77df94f739507aaeb5be444b0d34af7ee
Richard Pentecost <richard.pentecost@madetech.com>
2020-09-28 08:05:30 +0100

    refactor port tests with before(:each) hook

--- spec/port_spec.rb
+++ spec/port_spec.rb
@@ -1,31 +1,32 @@
 require_relative '../port'
 
 describe Port do
+  before(:each) do
+    @dover = described_class.new('Dover')
+  end
+
   context 'port' do
     it 'initiates a port with a name' do
-      calais = described_class.new('Calais')
-      expect(calais.name).to eq('Calais')
+      expect(@dover.name).to eq('Dover')
     end
 
     it 'adds a ship to a port' do
-      dover = described_class.new('Dover')
       ship = {}
 
-      dover.add_ship(ship)
-      expect(dover.ships).to include(ship)
+      @dover.add_ship(ship)
+      expect(@dover.ships).to include(ship)
     end
 
     it 'removes a ship from a port' do
-      dover = described_class.new('Dover')
       titanic = {ship: 'Titanic'}
       queenMary = {ship: 'Queen Mary'}
 
-      dover.add_ship(titanic)
-      dover.add_ship(queenMary)
-      dover.remove_ship(titanic)
+      @dover.add_ship(titanic)
+      @dover.add_ship(queenMary)
+      @dover.remove_ship(titanic)
  
-      expect(dover.ships).to_not include(titanic)
-      expect(dover.ships).to eq([queenMary])
+      expect(@dover.ships).to_not include(titanic)
+      expect(@dover.ships).to eq([queenMary])
     end
   end
 end
